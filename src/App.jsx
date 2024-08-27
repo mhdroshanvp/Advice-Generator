4a7985ea55473a74465e4cf958052b2c2d3d214d
roshan <roshanroshuff@gmail.com>
2024-08-28 00:06:01 +0530

    8/28/24 12:05

--- src/App.jsx
+++ src/App.jsx
@@ -2,39 +2,44 @@ import { useState } from 'react';
 import './App.css'
 
 function App() {
-  const [advice, setAdvice] = useState('GET YOUR ADVICE')
+  const [advice, setAdvice] = useState('GET YOUR ADVICE');
 
   async function getAdvice() {
-    const res = await fetch('https://api.adviceslip.com/advice')
-    const data = await res.json()
-    setAdvice(data?.slip?.advice)
+    const res = await fetch('https://api.adviceslip.com/advice');
+    const data = await res.json();
+    setAdvice(data?.slip?.advice);
   }
 
   function clearAdvice() {
-    setAdvice('GET YOUR ADVICE')
+    setAdvice('GET YOUR ADVICE');
   }
 
   return (
     <>
-      <div className='flex flex-col justify-center gap-y-5 h-screen items-center px-4 sm:px-8 md:px-12 lg:px-16 || relative group'>
-        <h2 className='font-bold text-black text-2xl sm:text-3xl md:text-4xl lg:text-5xl uppercase text-center'>
-          "{advice}"
-        </h2>
-        <button
-          onClick={getAdvice}
-          className='text-black bg-white p-2 sm:p-3 md:p-4 lg:p-5 hover:font-semibold uppercase border border-y-8 border-x-8 border-l-red-500 border-t-black border-b-green-500 border-r-white'
-        >
-          Click here for the advice
-        </button>
-        <button
-          onClick={clearAdvice}
-          className='text-black p-2 sm:p-3 md:p-4 lg:p-5 font-mono uppercase border border-black'
-        >
-          Clear
-        </button>
+      <div className='flex justify-center items-center h-screen px-4 sm:px-8 md:px-12 lg:px-16'>
+        <div className='w-full max-w-[600px] h-auto flex flex-col justify-center gap-5 items-center p-6 sm:p-8 md:p-10 lg:p-12 bg-black bg-opacity-15 rounded-lg'>
+          <h2 className='font-bold text-black text-xl sm:text-1xl md:text-2xl lg:text-3xl uppercase text-center'>
+            "{advice}"
+          </h2>
+          <button 
+            onClick={getAdvice} 
+            className='text-black bg-white p-1 sm:p-1 md:p-2 lg:p-3 hover:font-semibold uppercase border border-y-8 border-x-8 border-l-red-500 border-t-black border-b-green-500 border-r-white'>
+            Click here for the advice
+          </button>
+          <div className='flex flex-row gap-4 flex-wrap justify-center'>
+            <button className='text-black p-1 sm:p-1 md:p-2 lg:p-3 font-mono uppercase border border-black'>
+              Copy Advice
+            </button>
+            <button 
+              onClick={clearAdvice} 
+              className='text-black p-1 sm:p-1 md:p-2 lg:p-3 font-mono uppercase border border-black'>
+              Clear
+            </button>
+          </div>
+        </div>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
